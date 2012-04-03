4985b9ff79b9cb171b6d9ac9e6983cbff95842bb
Juan F Paulini <jpaulini@gmail.com>
2012-04-03 14:07:26 -0300

    Initial commit of codigo secreto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodigoSecreto::Application.config.secret_token = '6afce10fc472976f05b76b306bb5e529165def7a8406de710669bb9012ed2d078250c4f248c39c26f4478456b5f5c34fc09c44d54751509d4561bd1bde772154'

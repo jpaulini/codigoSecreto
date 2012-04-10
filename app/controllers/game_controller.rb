fbd74dc421817ed4aeae1571537015eb9ea54f1b
Juan F Paulini <jpaulini@gmail.com>
2012-04-10 16:34:36 -0300

    Failing game controller rspecs

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,8 +3,12 @@ class GameController < ApplicationController
   end
 
   def playing
-	@secret_code='ABCD'
-	if params[:firstCell] + params[:secondCell]+params[:thirdCell]+params[:fourthCell] == @secret_code
+	@round="XXXX"
+	if not params.nil?
+	  @round=params[:firstCell] + params[:secondCell]+params[:thirdCell]+params[:fourthCell]
+	end
+	@result=Game.check_code(@round)
+	if  @result == "4" 
 		redirect_to game_over_path :result => "success"
 	end
   end
--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -9,9 +9,9 @@ describe GameController do
     end
   end
 
-  describe "GET 'playing'" do
+  describe "POST 'playing'" do
     it "returns http success" do
-      get 'playing'
+      post 'playing'
       response.should be_success
     end
   end
@@ -22,5 +22,16 @@ describe GameController do
       response.should be_success
     end
   end
+
+	describe "Checking guesses against secret code" do
+	before :each do
+		@fake_game=mock('game',:secret_code => "ABCD")
+	end
+		it "should return true if the guess == secret code" do
+			Game.stub(:check_code).and_return("4")
+			response.should render_template('game_over')
+		end
+
+	end
 
 end
